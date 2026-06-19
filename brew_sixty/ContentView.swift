0c3246c38afe222bfa80f46893d43c27f78713f6
Charu7596 <gcharu7596@gmail.com>
2026-06-19 22:54:03 +0530

    feat: centralize gradient background token and force dark mode color scheme

--- brew_sixty/ContentView.swift
+++ brew_sixty/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
                 .transition(.opacity)
             }
         }
+        .preferredColorScheme(.dark)
     }
 }
 
--- brew_sixty/Model/Color+Theme.swift
+++ brew_sixty/Model/Color+Theme.swift
@@ -14,3 +14,15 @@ extension Color {
     /// Coffee Cream color for text and highlights
     static let coffeeCream = Color(red: 0.92, green: 0.85, blue: 0.78)
 }
+
+extension RadialGradient {
+    static var coffeeBackground: RadialGradient {
+        RadialGradient(
+            colors: [Color(red: 0.33, green: 0.16, blue: 0.09), Color(red: 0.10, green: 0.08, blue: 0.09)],
+            center: .center,
+            startRadius: 10,
+            endRadius: 500
+        )
+    }
+}
+
